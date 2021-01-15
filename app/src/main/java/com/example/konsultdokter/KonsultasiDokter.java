78621bbab8ff986fa43455b861f19a12cde81af8
geotirta <1711500015@mahasiswa.atmaluhur.ac.ud>
2021-01-14 20:56:45 -0800

    menu utama

---
+++ app/src/main/java/com/example/konsultdokter/KonsultasiDokter.java
@@ -0,0 +1,14 @@
+package com.example.konsultdokter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KonsultasiDokter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konsultasi_dokter);
+    }
+}
